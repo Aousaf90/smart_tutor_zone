2daa1bce885d251f5e23e531ece82b0890318791
Aousaf Sulaman <59832341+Aousaf90@users.noreply.github.com>
2024-03-04 14:25:17 +0500

    feature: CourseHelper Function for course outline

--- lib/Courses/courseHelper.dart
+++ lib/Courses/courseHelper.dart
@@ -1,41 +1,75 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-List<String> mainCategoriesList = [];
-Map<String, List<dynamic>> courseCollection = {};
+List mainCategory = [];
+getMainCategories() {
+  CollectionReference collectionRef =
+      FirebaseFirestore.instance.collection("Courses_Categories");
 
-Future<void> getAllCategories() async {
-  List<String> categories = [];
-
-  try {
-    CollectionReference mainCategoriesCollection =
-        FirebaseFirestore.instance.collection("Courses_Categories");
-
-    QuerySnapshot querySnapshot = await mainCategoriesCollection.get();
-    mainCategoriesList = querySnapshot.docs.map((docs) => docs.id).toList();
-
-    for (var mainCategory in mainCategoriesList) {
-      DocumentReference documentRef =
-          mainCategoriesCollection.doc(mainCategory);
-
-      documentRef.get().then(
-        (querySnapshot) {
-          final data = querySnapshot.data() as Map<String, dynamic>;
-          List subCategoryNames = data['subCategories'];
-          for (var subCategory in subCategoryNames) {
-            documentRef.collection(subCategory).get().then(
-              (subQuerySnapshot) {
-                subQuerySnapshot.docs.forEach(
-                  (doc) {
-                    print("${mainCategory} = ${subCategory} = ${doc.id}");
-                  },
-                );
-              },
-            );
-          }
+  collectionRef.get().then(
+    (value) {
+      value.docs.forEach(
+        (element) {
+          mainCategory.add(element.id);
         },
       );
+    },
+  );
+  Set<dynamic> allCategories = mainCategory.toSet();
+  return allCategories;
+}
+
+Future<dynamic> getSubCategories(document_name) async {
+  try {
+    List sub_categories = [];
+    CollectionReference collectionRef =
+        FirebaseFirestore.instance.collection("Courses_Categories");
+
+    final DocumentSnapshot snapshot =
+        await collectionRef.doc(document_name).get();
+
+    if (snapshot.exists) {
+      Map<String, dynamic> Document_Value =
+          snapshot.data() as Map<String, dynamic>;
+    } else {
+      print("Document does not exist");
     }
+    return snapshot['subCategories'];
   } catch (e) {
-    print('Error getting categories: $e');
+    print(
+      "${e.toString()}",
+    );
   }
+}
+
+Future<Map<String, dynamic>> getCourse(mainDocName, DocName) async {
+  final Map<String, dynamic> sub_Category_List = {};
+  List<dynamic> course_list = [];
+  final CollectionReference collectionReference = FirebaseFirestore.instance
+      .collection("/Courses_Categories/$mainDocName/$DocName");
+  await collectionReference.get().then(
+    (value) {
+      value.docs.forEach(
+        (element) {
+          course_list.add(element.id);
+        },
+      );
+    },
+  );
+  sub_Category_List[DocName] = course_list;
+  return sub_Category_List;
+}
+
+getAllCategories() async {
+  Map<String, dynamic> course_detail = {};
+  List main_categories = await getMainCategories().toList();
+  for (var categorie in main_categories) {
+    List sub_courses_list = [];
+    List subCategories = await getSubCategories(categorie);
+    for (var subCategor in subCategories) {
+      Map<String, dynamic> courses = await getCourse(categorie, subCategor);
+      sub_courses_list.add(courses);
+    }
+    course_detail[categorie] = sub_courses_list;
+  }
+  print(course_detail['Technology'][0]);
 }
