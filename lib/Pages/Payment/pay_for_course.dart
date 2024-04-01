53a65591815fdbb2500ef6a440ce82ba7fb4f27c
Aousaf Sulaman <59832341+Aousaf90@users.noreply.github.com>
2024-04-01 12:06:02 +0500

    feature: pay with card  method page completed

--- lib/Pages/Payment/pay_for_course.dart
+++ lib/Pages/Payment/pay_for_course.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_tutor_zone/Courses/selectedCourse.dart';
+import 'package:smart_tutor_zone/Pages/Payment/payment_with_card.dart';
 
-class CoursePayment extends StatelessWidget {
-  GestureDetector course_view_box;
-  CoursePayment({required this.course_view_box});
+class CoursePayment extends StatefulWidget {
+  @override
+  State<CoursePayment> createState() => _CoursePaymentState();
+}
+
+bool google_checkbox = false;
+bool card_checkbox = false;
+
+class _CoursePaymentState extends State<CoursePayment> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff5f9ff),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 30),
         child: Column(
@@ -30,10 +39,207 @@ class CoursePayment extends StatelessWidget {
               ),
             ),
             Container(
-              child: Container(),
-            )
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      padding: EdgeInsets.all(20),
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            height: 70,
+                            width: 70,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Graphic Design",
+                                  style: TextStyle(
+                                    color: Color(0xffff7c25),
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                const Text(
+                                  "Setup Your Graphic Design",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        const Text(
+                          "Select the Payment Methods you Want to Use",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        GooglePayContainer(
+                          is_checked: google_checkbox,
+                          change_value: (value) {
+                            setState(() {
+                              if (card_checkbox == true) {
+                                card_checkbox = !card_checkbox;
+                              }
+                              google_checkbox = value!;
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        PayWithDebitCardContainer(
+                          is_checked: card_checkbox,
+                          change_value: (value) {
+                            if (google_checkbox == true) {
+                              google_checkbox = !google_checkbox;
+                            }
+                            setState(() {
+                              card_checkbox = value!;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 90),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: SizedBox(
+                height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff005af5),
+                  ),
+                  onPressed: () {
+                    card_checkbox ? payWithCard(context) : payWithGooglePay();
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Enroll Course . /-",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: Icon(Icons.arrow_forward),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  payWithCard(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CardPayment(),
+      ),
+    );
+  }
+
+  payWithGooglePay() {
+    print("Pay with G Pay");
+  }
+}
+
+class PayWithDebitCardContainer extends StatelessWidget {
+  bool is_checked;
+  void Function(bool?)? change_value;
+  PayWithDebitCardContainer(
+      {required this.is_checked, required this.change_value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Text(
+            "Pay With Credit Card/ Debit Card",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Checkbox(
+            value: is_checked,
+            onChanged: change_value,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GooglePayContainer extends StatelessWidget {
+  bool is_checked = false;
+  void Function(bool?)? change_value;
+  GooglePayContainer({required this.is_checked, required this.change_value});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Text(
+            "Google Pay",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Checkbox(
+            value: is_checked,
+            onChanged: change_value,
+          ),
+        ],
       ),
     );
   }
---
+++ lib/Pages/Payment/payment_with_card.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CardPayment extends StatefulWidget {
+  @override
+  State<CardPayment> createState() => _CardPaymentState();
+}
+
+class _CardPaymentState extends State<CardPayment> {
+  final _globalKey = GlobalKey<FormState>();
+
+  TextStyle cardTextStyle = TextStyle(
+    color: Colors.white,
+  );
+  String card_holder_name = "Card Number";
+  String card_number = "Holder Name";
+  String expiration_date = "MM/YY";
+  String security_code = "CVV";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Card Details",
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 200,
+              width: 350,
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: AssetImage("images/credit_card_texture.jpg"),
+                    opacity: 0.9,
+                    fit: BoxFit.cover),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  Text(
+                    card_number,
+                    style: cardTextStyle,
+                  ),
+                  const SizedBox(height: 50),
+                  Text(
+                    card_holder_name,
+                    style: cardTextStyle,
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        expiration_date,
+                        style: cardTextStyle,
+                      ),
+                      const SizedBox(width: 70),
+                      Text(
+                        security_code,
+                        style: cardTextStyle,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Form(
+                key: _globalKey,
+                child: Column(
+                  children: [
+                    CardDetailListTile(
+                      required_field: "Card Number",
+                      validationCallBack: (value) {
+                        if (RegExp(r'^.*[a-zA-Z].*$').hasMatch(value!) &&
+                                value.length < 16 ||
+                            value.length > 16) {
+                          return "Invalid Card Number";
+                        } else {
+                          return null;
+                        }
+                      },
+                      onChangedCallBack: (value) {
+                        setState(
+                          () {
+                            card_number = value;
+                          },
+                        );
+                      },
+                    ),
+                    CardDetailListTile(
+                      required_field: "Name",
+                      validationCallBack: (value) {
+                        if (value!.isEmpty == true) {
+                          return "Field Can Not be Empty";
+                        }
+                      },
+                      onChangedCallBack: (value) {
+                        setState(
+                          () {
+                            value = value.toUpperCase();
+                            card_holder_name = value;
+                          },
+                        );
+                      },
+                    ),
+                    CardDetailListTile(
+                      required_field: "Expiration Date",
+                      validationCallBack: (value) {
+                        if (RegExp(r'^.*[a-zA-Z].*$').hasMatch(value!) &&
+                                value.length < 4 ||
+                            value.length > 4) {
+                          return "Invalid Card Number";
+                        } else {
+                          return null;
+                        }
+                      },
+                      onChangedCallBack: (value) {
+                        setState(
+                          () {
+                            String slash = "/";
+                            if (value.length == 2) {
+                              expiration_date = value + slash;
+                            } else if (value.length > 2) {
+                              expiration_date = value.substring(0, 2) +
+                                  slash +
+                                  value.substring(2);
+                            } else {
+                              expiration_date = value;
+                            }
+                          },
+                        );
+                      },
+                    ),
+                    CardDetailListTile(
+                      required_field: "Security Code",
+                      validationCallBack: (value) {
+                        if (RegExp(r'^.*[a-zA-Z].*$').hasMatch(value!) &&
+                                value.length < 3 ||
+                            value.length > 3) {
+                          return "Invalid Security code";
+                        }
+                      },
+                      onChangedCallBack: (value) {
+                        setState(
+                          () {
+                            security_code = value;
+                          },
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xff005af5),
+              ),
+              width: 350,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Pay Amount",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardDetailListTile extends StatelessWidget {
+  String required_field;
+  void Function(String)? onChangedCallBack;
+  String? Function(String?)? validationCallBack;
+  CardDetailListTile(
+      {required this.required_field,
+      required this.onChangedCallBack,
+      required this.validationCallBack});
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return ListTile(
+      leading: Container(
+        width: 130,
+        child: Text(
+          required_field,
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+      title: Container(
+        child: TextFormField(
+          decoration: InputDecoration(hintText: "Required"),
+          validator: validationCallBack,
+          onChanged: onChangedCallBack,
+        ),
+      ),
+    );
+  }
+
+  payAmount() {}
+}
